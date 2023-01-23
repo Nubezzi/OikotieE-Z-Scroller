bd2784f87c93636618ef6fc4e8c2f6a2de4f2391
Nubezzi <patenur@hotmail.com>
2023-01-24 01:12:12 +0200

    initial test for the scroll to simulate arrow key presses. Oikoties seems to have the eventlistener somewhere where i cant get to it :D

--- image-scroller.js
+++ image-scroller.js
@@ -1,14 +1,54 @@
 let scrollingEnabled = true
+var scrollingType = true //false for button scrolling, true for simulated arrow keys
 let right = document.getElementsByClassName('icon icon--chevron-right icon--gallery')
 let left = document.getElementsByClassName('icon icon--chevron-left icon--gallery')
+let gallery = document.getElementsByClassName('galleria')
 
 chrome.storage.onChanged.addListener((changes, namespace) => {
     for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
       if(key == "scrollingEnabled"){
         scrollingEnabled = newValue
       }
+      if(key == "scrollingType"){
+        scrollingType = newValue
+      }
     }
-  });
+});
+
+document.addEventListener('keydown', function(event) {
+    const key = event.key; // "ArrowRight", "ArrowLeft", "ArrowUp", or "ArrowDown"
+    console.log(key)
+})
+
+function fireKey(el){
+    var keycode;
+    var key;
+    switch(el)
+    {
+        case "left":
+        keycode = 37;
+        key = "ArrowLeft"
+        break;
+        case "right":
+        keycode = 39;
+        key = "ArrowRight"
+        break;    
+    }
+    if(document.createEventObject)
+    {
+        var eventObj = document.createEventObject();
+        eventObj.keyCode = key;
+        document.fireEvent("onkeydown", eventObj);   
+    }else if(document.createEvent)
+    {
+        var eventObj = document.createEvent("Event");
+        eventObj.initEvent('keydown', true, true);
+        eventObj.which = keycode;
+        eventObj.key = key
+        document.dispatchEvent(eventObj)
+        //console.log(key)
+    } 
+}
 
 function startScrolling() {
     // add event listener for mouse wheel scroll
@@ -24,10 +64,23 @@ function stopScrolling() {
 function handleScroll(event) {
     if (event.deltaY < 0) {
         // mouse wheel scrolled up
-        right[0].click()
+        if(scrollingType){
+            //console.log("oikea")
+            fireKey("right")
+            //document.dispatchEvent(new KeyboardEvent('keydown', {'which': 39}));
+        }else{
+            right[0].click()
+        }
     } else {
         // mouse wheel scrolled down
-        left[0].click()
+        if(scrollingType){
+            //console.log("vasen")
+            fireKey("left")
+            //document.dispatchEvent(new KeyboardEvent('keydown', {'which': 37}));
+            //fireKey("left")
+        }else{
+            left[0].click()
+        }
     }
 }
 
--- popup.js
+++ popup.js
@@ -1,7 +1,11 @@
 var scrollingEnabled = true;
+var scrollingType = true //false for button scrolling, true for simulated arrow keys
 chrome.storage.local.set({scrollingEnabled: scrollingEnabled}, function() {});
+chrome.storage.local.set({scrollingType: scrollingType}, function() {});
 let checker = document.getElementById("scrolling-toggle")
+let checker2 = document.getElementById("type-toggle")
 checker.addEventListener("change", toggleScrolling);
+checker2.addEventListener("change", toggleType);
 
 if(document.getElementById("scrolling-toggle")){
     console.log("löyty")
@@ -10,5 +14,15 @@ if(document.getElementById("scrolling-toggle")){
 function toggleScrolling() {
     scrollingEnabled = !scrollingEnabled;
     chrome.storage.local.set({scrollingEnabled: scrollingEnabled}, function() {});
+}
+
+function toggleType() {
+    scrollingType = !scrollingType;
+    chrome.storage.local.set({scrollingType: scrollingType}, function() {});
+    if(scrollingType){
+        document.getElementById("type-toggle-span").innerHTML = "<- Toggle, Simulated Arrow keys";
+    }else{
+        document.getElementById("type-toggle-span").innerHTML = "<- Toggle, DOM Button";
+    }
 }
 
